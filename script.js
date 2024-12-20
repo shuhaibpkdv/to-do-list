9a83954cc4ed4b4475006e727bb4e7c1845f2574
muhammed-shuhaib <muhammedshuhaibpkdv@gmail.com>
2024-12-20 19:26:10 +0530

    added script and compleated project

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#addTask").click(function () {
+
+        let inpValue = $("#inpValue").val().trim();
+
+        if (inpValue !== "") {
+            taskItem = `
+               <li class="list-group-item d-flex justify-content-between align-items-center">
+                  ${inpValue}
+                  <button class="btn btn-danger btn-sm deleteTask">Delete</button>
+               </li>
+            `;
+
+            $(".tasklist").append(taskItem);
+
+            $("#inpValue").val("");
+
+        }else {
+            alert("fuckYouuu man");
+        }
+
+    })
+
+    $(document).on("click", ".deleteTask", function() {
+        $(this).closest("li").remove(); // Remove the parent <li> of the clicked button
+    });
+    
+})
